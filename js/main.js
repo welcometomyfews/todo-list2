7739b1469dddb02fe999904eb05d7e1a59c57578
Menyx <91610907+welcometomyfews@users.noreply.github.com>
2024-10-22 09:12:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 // Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBY_1eBY5KQc-3mITZ7iybYAg8hey0HYnQ",
